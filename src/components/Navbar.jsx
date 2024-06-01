efd9f23b78ab7eba0787c653902b92fc0e614a6e
“Mynorka <mynorkadaza@gmail.com>
2024-06-01 11:58:08 +0200

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+function Navbar(){
+    return(
+        <div className="Navbar">
+        <img src="" alt="logo"/>
+        <div>
+            <p>Home</p>
+            <p>Books</p>
+            <p>About Us</p>
+        </div>
+        <button> My profile</button>
+
+        </div>
+    )
+
+}
+
+export default Navbar
