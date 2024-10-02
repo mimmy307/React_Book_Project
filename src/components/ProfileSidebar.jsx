1182de6651907494065d428eaf833c7745b2ba61
“Mynorka <mynorkadaza@gmail.com>
2024-10-02 12:59:04 +0200

    profile section responsive

--- src/components/ProfileSidebar.jsx
+++ src/components/ProfileSidebar.jsx
@@ -1,14 +1,14 @@
 import { NavLink} from "react-router-dom"
 import "../components/ProfileSidebar.css"
 
-function ProfileSidebar(){
+function ProfileSidebar({closeSidebar, isVisible}){
 
     return(
-        <div className="profile-sidebar-main">
+        <div className={`profile-sidebar-main ${isVisible ? 'visible' : ''}`}>
             <h3>My Profile</h3>
-            <NavLink to="mydetails" className= {({isActive}) => isActive ? "profile-link profile-active" : "profile-link"} ><p>My details</p></NavLink>
-            <NavLink to="myreadinglist" className= {({isActive}) => isActive ? "profile-link profile-active" : "profile-link"}><p>My reading list</p></NavLink>
-            <NavLink to="addbooks" className= {({isActive}) => isActive ? "profile-link profile-active" : "profile-link"}><p>Add books</p></NavLink>
+            <NavLink to="mydetails" className= {({isActive}) => isActive ? "profile-link profile-active" : "profile-link"} onClick={closeSidebar} ><p>My details</p></NavLink>
+            <NavLink to="myreadinglist" className= {({isActive}) => isActive ? "profile-link profile-active" : "profile-link"} onClick={closeSidebar}><p>My reading list</p></NavLink>
+            <NavLink to="addbooks" className= {({isActive}) => isActive ? "profile-link profile-active" : "profile-link"} onClick={closeSidebar}><p>Add books</p></NavLink>
         </div>
     )
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import AddBooksForm from "../components/AddBooksForm"
 import MyDetails from "../components/MyDetails"
 import ProfileSidebar from "../components/ProfileSidebar"
@@ -6,10 +7,22 @@ import "../pages/Profile.css"
 import { Routes, Route } from "react-router-dom"
 
 function Profile ({readingList, addBookToMyBooks}){
+    const [sidebarVisible, setSidebarVisible] = useState(false);
+
+    const toggleSidebar = () => {
+      setSidebarVisible(!sidebarVisible);
+    };
+  
+    const closeSidebar = () => {
+      setSidebarVisible(false); 
+    };
 
     return(
         <div className="profile-container">
-            <ProfileSidebar />
+            <button className="profile-menu" onClick={toggleSidebar}>
+                ☰
+            </button>
+            <ProfileSidebar  closeSidebar={closeSidebar} isVisible={sidebarVisible}/>
             <Routes>
             <Route path="/mydetails" element={<MyDetails />} />
             <Route path="/myreadinglist" element={<ReadingList  readingList={readingList}/> }/>
